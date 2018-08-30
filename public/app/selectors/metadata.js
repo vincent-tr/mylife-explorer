c003e866309e562a81baae8d5fe6b628f7d4de00
vincent-tr <vincent@stoic.com>
2018-08-30 09:10:22 +0200

    sort directory content

--- public/app/selectors/metadata.js
+++ public/app/selectors/metadata.js
@@ -3,4 +3,32 @@
 export const getMetadataPath    = state => state.metadata && state.metadata.path;
 export const getMetadataType    = state => state.metadata && state.metadata.type;
 export const getMetadataNodes   = state => state.metadata && state.metadata.nodes;
-export const getMetadataContent = state => state.metadata && state.metadata.content;
+export const getMetadataContent = state => state.metadata && sortContent(state.metadata.content);
+
+function sortContent(content) {
+  if(!content) {
+    return content;
+  }
+
+  const result = [ ... content ];
+  result.sort(contentComparer);
+  return result;
+}
+
+function contentComparer(a, b) {
+  if(a.type !== 'Directory' && b.type === 'Directory') {
+    return 1;
+  }
+  if(a.type === 'Directory' && b.type !== 'Directory') {
+    return -1;
+  }
+
+  if(a.name < b.name) {
+    return -1;
+  }
+  if(a.name > b.name) {
+    return 1;
+  }
+
+  return 0;
+}
